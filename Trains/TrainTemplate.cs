83fc8eddee2c13970d28c46160e1c46d7fefa523
Michal Vala <michal.vala@gmail.com>
2021-04-29 22:47:39 +0200

    train rides on rails \o/

--- Trains/TrainTemplate.cs
+++ Trains/TrainTemplate.cs
@@ -5,13 +5,34 @@ public class TrainTemplate : Spatial, PlayObject {
   [Export] public PlayColor color { get; private set; } = PlayColor.NONE;
 
   private Vector3 velocity = new Vector3(0, 0, 0);
+  private PathElement currentTarget;
+  private PathElement finalTarget;
+  private bool move = false;
 
   public override void _Ready() {
   }
 
   public override void _PhysicsProcess(float delta) {
     base._PhysicsProcess(delta);
-    Translate(velocity * delta);
+    if (move) {
+      Translate(velocity * delta);
+      var trainPos = GlobalTransform.origin;
+      trainPos.y = 0;
+      var targetPos = ((Spatial)currentTarget.item).GlobalTransform.origin;
+      targetPos.y = 0;
+      var distance = trainPos.DistanceTo(targetPos);
+      // GD.Print(string.Format("train: '{0}', target: '{1}', distance: '{2}'", trainPos, targetPos, distance));
+      if (distance < 0.01) {
+        if (currentTarget.next != null) {
+          currentTarget = currentTarget.next;
+        } else {
+          GD.Print(string.Format("{0} train arrived to end depot", color));
+          move = false;
+        }
+      } else {
+        LookAt(targetPos, new Vector3(0, 1, 0));
+      }
+    }
   }
 
   public void chooChoo(bool emit = true) {
@@ -22,7 +43,10 @@ public class TrainTemplate : Spatial, PlayObject {
   // TODO: ok we can't do it like this, I guess trains should go on rails
   public void go(PathElement from, PathElement to) {
     GD.Print(string.Format("train '{0}' should go from '{1}' to '{2}'", color, from, to));
-    velocity = new Vector3(0, 0, 1);
+    velocity = new Vector3(0, 0, -1);
+    currentTarget = from;
+    finalTarget = to;
+    move = true;
     // GlobalTransform = destination;
   }
 }
