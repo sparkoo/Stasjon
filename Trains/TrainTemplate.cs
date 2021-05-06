90e040d364199869b01ae7d08006f6bd9df35589
Michal Vala <michal.vala@gmail.com>
2021-05-06 07:13:22 +0200

    smoother ride

--- Trains/TrainTemplate.cs
+++ Trains/TrainTemplate.cs
@@ -1,31 +1,35 @@
-using System;
+using System.Collections.Generic;
 using Godot;
 
 public class TrainTemplate : Spatial, PlayObject {
   [Export] public PlayColor color { get; private set; } = PlayColor.NONE;
 
-  private Vector3 velocity = new Vector3(0, 0, 0);
-  private PathElement currentTarget;
-  private PathElement finalTarget;
+  private readonly float SPEED = 0.5F;
+  private readonly float DISTANCE_DELTA = 0.01F;
+
+  private Vector3 velocity;
+  private Queue<Vector3> trainRoute;
   private bool move = false;
 
   public override void _Ready() {
+    velocity = new Vector3(0, 0, -SPEED);
   }
 
   public override void _PhysicsProcess(float delta) {
     base._PhysicsProcess(delta);
     if (move) {
       Translate(velocity * delta);
+
       var trainPos = GlobalTransform.origin;
       trainPos.y = 0;
-      var targetPos = ((Spatial)currentTarget.item).GlobalTransform.origin;
+
+      var targetPos = trainRoute.Peek();
       targetPos.y = 0;
+
       var distance = trainPos.DistanceTo(targetPos);
-      // GD.Print(string.Format("train: '{0}', target: '{1}', distance: '{2}'", trainPos, targetPos, distance));
-      if (distance < 0.01) {
-        if (currentTarget.next != null) {
-          currentTarget = currentTarget.next;
-        } else {
+      if (distance < DISTANCE_DELTA) {
+        trainRoute.Dequeue();
+        if (trainRoute.Count == 0) {
           GD.Print(string.Format("{0} train arrived to end depot", color));
           move = false;
         }
@@ -40,13 +44,28 @@ public class TrainTemplate : Spatial, PlayObject {
     ((Particles)GetNode("Smoke2")).Emitting = emit;
   }
 
-  // TODO: ok we can't do it like this, I guess trains should go on rails
   public void go(PathElement from, PathElement to) {
     GD.Print(string.Format("train '{0}' should go from '{1}' to '{2}'", color, from, to));
-    velocity = new Vector3(0, 0, -1);
-    currentTarget = from;
-    finalTarget = to;
+    prepareTrainRoute(from, to);
     move = true;
     // GlobalTransform = destination;
+  }
+
+  private void prepareTrainRoute(PathElement from, PathElement to) {
+    trainRoute = new Queue<Vector3>();
+    var currentPathElement = from;
+    while (currentPathElement != null) {
+      if (currentPathElement.item is RailsTemplate) {
+        var rails = (RailsTemplate)currentPathElement.item;
+        foreach (Position3D point in rails.GetNode("NavPoints").GetChildren()) {
+          trainRoute.Enqueue(point.GlobalTransform.origin);
+        }
+      }
+      if (currentPathElement.item is DepotTemplate) {
+        var depot = (DepotTemplate)currentPathElement.item;
+        trainRoute.Enqueue(depot.GlobalTransform.origin);
+      }
+      currentPathElement = currentPathElement.next;
+    }
   }
 }
